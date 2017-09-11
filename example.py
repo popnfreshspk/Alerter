3644decbcfee2c32cc85915e3129faebf9d68628
popnfreshspk <kwkhsu@gmail.com>
2017-09-11 16:23:09 -0400

    adding example file

---
+++ example.py
@@ -0,0 +1,24 @@
+from Alerter import alerter
+
+default_directory = '~/'
+
+@alerter(name='passing_alert', path=default_directory)
+def passing_alert():
+	return False
+
+
+@alerter(name='failing_alert', path=default_directory)
+def failing_alert():
+	return True
+
+
+@alerter(name='exception_alert', path=default_directory)
+def exception_alert():
+	return 1/0
+
+
+if __name__ == '__main__':
+	passing_alert()
+	failing_alert()
+	exception_alert()
+
